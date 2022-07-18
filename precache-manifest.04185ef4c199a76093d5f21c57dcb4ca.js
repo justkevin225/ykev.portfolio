ea95a26b50588c275f0e5735efdee5840945416c
justkevin225 <kevinkouakou113@gmail.com>
2022-07-18 12:52:39 +0000

    Updates

---
+++ precache-manifest.04185ef4c199a76093d5f21c57dcb4ca.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1846f29f54a25779ed0ab299a61e2a",
+    "url": "/ykev.portfolio/index.html"
+  },
+  {
+    "revision": "f43c4358a140ab27f47e",
+    "url": "/ykev.portfolio/static/css/4.68a81a89.chunk.css"
+  },
+  {
+    "revision": "1c401bc4c9d8c819851b",
+    "url": "/ykev.portfolio/static/css/main.c3475f22.chunk.css"
+  },
+  {
+    "revision": "cf24bd0d78fee732c50a",
+    "url": "/ykev.portfolio/static/js/0.d032e0b3.chunk.js"
+  },
+  {
+    "revision": "5ca2cb7d02e3b9b29f38",
+    "url": "/ykev.portfolio/static/js/10.0fcb5eeb.chunk.js"
+  },
+  {
+    "revision": "2acaa1d013d23809ea81",
+    "url": "/ykev.portfolio/static/js/3.b491fb32.chunk.js"
+  },
+  {
+    "revision": "f43c4358a140ab27f47e",
+    "url": "/ykev.portfolio/static/js/4.c143b7f7.chunk.js"
+  },
+  {
+    "revision": "e59aafd68e2dda11a915",
+    "url": "/ykev.portfolio/static/js/5.a6023c18.chunk.js"
+  },
+  {
+    "revision": "cb172917386f6ad738a1",
+    "url": "/ykev.portfolio/static/js/6.d1b86aea.chunk.js"
+  },
+  {
+    "revision": "fdb1c3faeda7883eb8d6",
+    "url": "/ykev.portfolio/static/js/7.c0a18fa0.chunk.js"
+  },
+  {
+    "revision": "81f58229eef21da490d2",
+    "url": "/ykev.portfolio/static/js/8.de3dc942.chunk.js"
+  },
+  {
+    "revision": "f4d9ab64bd3720acb647",
+    "url": "/ykev.portfolio/static/js/9.fc064622.chunk.js"
+  },
+  {
+    "revision": "1c401bc4c9d8c819851b",
+    "url": "/ykev.portfolio/static/js/main.03de95a6.chunk.js"
+  },
+  {
+    "revision": "7edb836a0525b616fa32",
+    "url": "/ykev.portfolio/static/js/runtime-main.b694fd07.js"
+  },
+  {
+    "revision": "f915b89943158a5858b6650cc6076978",
+    "url": "/ykev.portfolio/static/media/just_me.f915b899.png"
+  },
+  {
+    "revision": "6346abe98943e3439110e756812cbac4",
+    "url": "/ykev.portfolio/static/media/mon-logo-bg-transparent.6346abe9.png"
+  }
+]);
