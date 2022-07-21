3a4a68ddbf9d2378c163174c8705187b8e50b9cf
justkevin225 <kevinkouakou113@gmail.com>
2022-07-21 14:31:48 +0000

    v0.1 deployable de mon portfolio

--- src/helpers/checkForShowNav.ts
+++ src/helpers/checkForShowNav.ts
@@ -1,11 +1,22 @@
 export const checkForShowNav = (): boolean => {
   const path: string = window.location.pathname;
-  return path === "/Accueil" ||
-    path === "/Portfolio" ||
-    path === "/Moi" ||
-    path === "/Contact" ||
-    path === "/Skills" ||
-    path === "/"
+  const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+  if (isDev) {
+    return path === "/Accueil" ||
+      path === "/Portfolio" ||
+      path === "/Moi" ||
+      path === "/Contact" ||
+      path === "/Skills" ||
+      path === "/"
+      ? true
+      : false;
+  }
+  return path === "ykev.portfolio/Accueil" ||
+    path === "ykev.portfolio/Portfolio" ||
+    path === "ykev.portfolio/Moi" ||
+    path === "ykev.portfolio/Contact" ||
+    path === "ykev.portfolio/Skills" ||
+    path === "ykev.portfolio/"
     ? true
     : false;
 };
